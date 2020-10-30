c549a3d090e114c5f69b8edc4b19f244cd1f2a26
ShravyaHubb <shravyak146@gmail.com>
2020-10-30 16:00:30 +0530

    ADDED: code to test linkedlist with three nodes appended to it

--- test/java/MyLinkedListTest.java
+++ test/java/MyLinkedListTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class MyLinkedListTest {
 
     @Test
-    public void givenThreeNumbers_WhenAddedToLinkedList_ShouldBeAddedToTop() {
+    public void givenThreeNodes_WhenAddedToLinkedList_ShouldBeAddedToTop() {
         MyNode<Integer> myFirstNode = new MyNode<>(70);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
         MyNode<Integer> myThirdNode = new MyNode<>(56);
@@ -14,6 +14,20 @@ public class MyLinkedListTest {
         myLinkedList.add(myThirdNode);
         myLinkedList.printMyNodes();
         boolean result = myLinkedList.head.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myFirstNode);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenThreeNodes_WhenAppendedToLinkedList_ShouldBeAddedToLast() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myThirdNode);
         Assert.assertTrue(result);
     }
 
