f804855d752e1cda8078d73d01e570f4174ed6e0
ShravyaHub <shravyak710@gmail.com>
2020-11-02 11:22:11 +0530

    ADDED: code to test linkedhashmap

--- test/java/MyLinkedHashMapTest.java
+++ test/java/MyLinkedHashMapTest.java
@@ -1,6 +1,8 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class MyLinkedHashMapTest {
 
     @Test
@@ -17,7 +19,25 @@ public class MyLinkedHashMapTest {
         }
         System.out.println(myLinkedHashMap);
         int frequency = myLinkedHashMap.get("paranoid");
-        Assert.assertEquals(3, frequency);
+        assertEquals(3, frequency);
+    }
+
+    @Test
+    public void givenParagraph_WhenWordIsRemoved_ShouldReturnNull() {
+        String sentence = "Paranoids are not paranoid because "
+                + "they are paranoid but because they keep putting themselves deliberately into "
+                + "paranoid avoidable situations";
+        MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+        String[] words = sentence.toLowerCase().split(" ");
+        for(String word : words) {
+            Integer value = myLinkedHashMap.get(word);
+            if(value == null) value = 1;
+            else value = value + 1;
+            myLinkedHashMap.add(word, value);
+        }
+        myLinkedHashMap.remove("avoidable");
+        System.out.println(myLinkedHashMap);
+        Assert.assertEquals(null, myLinkedHashMap.get("avoidable"));
     }
 
 }
